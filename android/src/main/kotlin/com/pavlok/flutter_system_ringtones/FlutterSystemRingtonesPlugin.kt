c0b49e4ecb560e3547adea1a2c1f171592444432
Imed Boumalek <imedboumalek@gmail.com>
2022-07-06 14:23:31 +0100

    updated finished loading ringtones on android side

--- android/src/main/kotlin/com/pavlok/flutter_system_ringtones/FlutterSystemRingtonesPlugin.kt
+++ android/src/main/kotlin/com/pavlok/flutter_system_ringtones/FlutterSystemRingtonesPlugin.kt
@@ -1,5 +1,8 @@
 package com.pavlok.flutter_system_ringtones
 
+import android.content.Context
+import android.media.Ringtone
+import android.media.RingtoneManager
 import androidx.annotation.NonNull
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
@@ -9,27 +12,49 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 /** FlutterSystemRingtonesPlugin */
-class FlutterSystemRingtonesPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+class FlutterSystemRingtonesPlugin : FlutterPlugin, MethodCallHandler {
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private lateinit var channel: MethodChannel
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_system_ringtones")
-    channel.setMethodCallHandler(this)
-  }
+    private lateinit var ringtoneManager: RingtoneManager
+    val ringtones = arrayListOf<Ringtone>()
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        loadRingtones(flutterPluginBinding.applicationContext)
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_system_ringtones")
+        channel.setMethodCallHandler(this)
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        if (call.method == "getPlatformVersion") {
+            result.success("Android ${android.os.Build.VERSION.RELEASE}")
+        } else {
+            result.notImplemented()
+        }
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        emptyRingtones()
+        channel.setMethodCallHandler(null)
+    }
+
+    fun loadRingtones(context: Context): Unit {
+        ringtoneManager = RingtoneManager(context)
+        if (!ringtoneManager.cursor.isFirst) ringtoneManager.cursor.moveToFirst()
+        do {
+            val temp = ringtoneManager.getRingtone(ringtoneManager.cursor.position)
+            ringtones.add(temp)
+            println(temp)
+        } while (ringtoneManager.cursor.moveToNext())
+        ringtoneManager.cursor.close();
+    }
+
+
+    fun emptyRingtones(): Unit {
+        ringtones.clear();
+    }
 }
