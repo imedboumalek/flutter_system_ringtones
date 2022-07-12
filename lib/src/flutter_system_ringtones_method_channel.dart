06ea444260be7111ac95c57328847d30985a9567
Imed Boumalek <imedboumalek@gmail.com>
2022-07-12 15:44:22 +0100

    added some docs

--- lib/src/flutter_system_ringtones_method_channel.dart
+++ lib/src/flutter_system_ringtones_method_channel.dart
@@ -10,6 +10,8 @@ class MethodChannelFlutterSystemRingtones extends FlutterSystemRingtonesPlatform
   @visibleForTesting
   final methodChannel = const MethodChannel('flutter_system_ringtones');
 
+  /// invokes the getRingtones method on the native platform, which returns a list of hashmaps that
+  /// get converted to [Ringtone] objects containing platform ringtone sounds.
   @override
   Future<List<Ringtone>?> getRingtones() async {
     var ringtones = await methodChannel.invokeMethod<List>('getRingtones');
@@ -17,6 +19,8 @@ class MethodChannelFlutterSystemRingtones extends FlutterSystemRingtonesPlatform
     return ringtones?.map((map) => Ringtone.fromMap(map)).toList();
   }
 
+  /// invokes the getAlarms method on the native platform, which returns a list of hashmaps that
+  /// get converted to [Ringtone] objects containing platform alarm sounds.
   @override
   Future<List<Ringtone>?> getAlarms() async {
     var alarms = await methodChannel.invokeMethod<List>('getAlarms');
@@ -24,6 +28,8 @@ class MethodChannelFlutterSystemRingtones extends FlutterSystemRingtonesPlatform
     return alarms?.map((map) => Ringtone.fromMap(map)).toList();
   }
 
+  /// invokes the getNotifications method on the native platform, which returns a list of hashmaps that
+  /// get converted to [Ringtone] objects containing platform notification sounds.
   @override
   Future<List<Ringtone>?> getNotifications() async {
     var notifications = await methodChannel.invokeMethod<List>('getNotifications');
